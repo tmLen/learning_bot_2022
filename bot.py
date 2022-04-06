08d18a4ec323232e8cd5adbe119adaa705937426
Eugene Stepanov <evgen.stepanov.911@gmail.com>
2022-04-06 08:08:48 +0300

    added logging

--- bot.py
+++ bot.py
@@ -1,13 +1,18 @@
+import logging
 # Updater - компонент отвечающий за обмен данными с сервером Телеграм
 # CommandHandler - компонет отвечающий за обрабтку комманд
-
 from telegram.ext import CommandHandler, Updater
+
+logging.basicConfig(filename='bot.log', level=logging.INFO)
 
 key = '5217524414:AAHNL_XsbLKgkqZcTd-Bl41xKLFRNIzMqYI'
 
 #функция которая выводит приветствие пользователю при команде /start
 def greet_user(update, context):
-    print('hello!')
+    print('pressed /start!')
+    update.message.reply_text('Добрый день!')
+    print(update)
+    print(context)
 
 def main():
     #создаем апдейтер
